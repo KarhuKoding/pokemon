aec00158c4ffe5c98c285dc31daf019c80fd5c3c
JOPI-RTX3060\johan <johannes@walenta.dev>
2023-04-16 17:10:19 +0300

    render pokemondetails on detail page

--- src/pages/[pokemon].tsx
+++ src/pages/[pokemon].tsx
@@ -1,48 +1,78 @@
-import React from "react";
+import React, { Fragment } from "react";
 import { useRouter } from "next/router";
 import useSWR from "swr";
 import { API_BASE_URL } from "@/misc/constants";
+import { get } from "lodash";
 
-interface PokemonDetailData  {
-    species: string;
-    stats: Array<any>;
-    types: Array<any>;
-    weight: number;
-    moves: Array<any>;
+interface PokemonDetailData {
+  species: string;
+  stats: Array<any>;
+  types: Array<any>;
+  weight: number;
+  moves: Array<any>;
 }
 
+function getDetails(data: any): PokemonDetailData {
+  return {
+    species: get(data, "species.name", "no species"),
+    stats: get(data, "stats", []),
+    types: get(data, "types", []),
+    weight: get(data, "weight", 0),
+    moves: get(data, "moves", []),
+  };
+}
 
 export default function DetailPage() {
   const router = useRouter();
-  const name = router.query.pokemon;
+  const pokemonName = router.query.pokemon;
 
   const fetcher = (url: string) => fetch(url).then((r) => r.json());
 
-  if (!name) return <div>Something went wrong !</div>;
+  if (!pokemonName) return <div>Something went wrong !</div>;
 
   // TODO rewrite to NEXT.JS
-  const url = API_BASE_URL + "pokemon/" + name;
+  const url = API_BASE_URL + "pokemon/" + pokemonName;
   const { data, error, isLoading } = useSWR(url, fetcher);
 
   if (error) return <div>failed to load</div>;
   if (isLoading) return <div>loading...</div>;
 
-
-
-  console.log(data);
-
-// - Species // data.species.name: string
-// - Stats  //data.stats : array
-// - Types //data.types : array
-// - Weight //data.weight : number
-// - Moves  //data.abilities : array
+  const pokemonDetails = getDetails(data);
 
   return (
     <div>
       <div onClick={() => router.back()} style={{ cursor: "pointer" }}>
         <h1>&#8592; Go Back</h1>
       </div>
-      {name}
+      <h1>{pokemonName}</h1>
+      <section>
+        <div>Species: {pokemonDetails.species}</div>
+        <ul>
+          Stats:
+          {pokemonDetails.stats.map((stat) => {
+            return (
+              <li key={stat.stat.name}>
+                <strong>{stat.stat.name} </strong>Base: {stat.base_stat},
+                Effort: {stat.effort}
+              </li>
+            );
+          })}
+        </ul>
+        <ul>
+          Types:
+          {pokemonDetails.types.map((type) => {
+            return <li key={type.type.name}>{type.type.name}</li>;
+          })}
+        </ul>
+        <div>Weight: {pokemonDetails.weight}</div>
+        <p>
+          Moves:
+          <br />
+          {pokemonDetails.moves.map((move) => {
+            return <span key={move.move.name}>{move.move.name} /</span>;
+          })}
+        </p>
+      </section>
     </div>
   );
 }
