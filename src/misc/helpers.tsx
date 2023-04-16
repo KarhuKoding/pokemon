46cb55102a01154e426c0baf4b738306930c3c29
JOPI-RTX3060\johan <johannes@walenta.dev>
2023-04-16 18:10:41 +0300

    refcatoring, finished pagination logic

--- src/misc/helpers.tsx
+++ src/misc/helpers.tsx
@@ -1,3 +1,11 @@
 export function generateOffsetUrl(offset: number = 0): string {
   return `pokemon?limit=15&offset=${offset}`;
 }
+
+export function getTotalPagesForPagination(count: number): number {
+  return Math.ceil(count / 15);
+}
+
+export function getPokemonIndexFromUrl(url: string): string {
+  return url.split("/").reverse()[1];
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,6 @@
 import Head from "next/head";
 import { useRouter } from "next/router";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import {
   SimpleGrid,
   Card,
@@ -11,17 +11,36 @@ import {
 } from "@mantine/core";
 import { API_BASE_URL } from "../misc/constants";
 import { Pagination } from "@mantine/core";
-import { TOTAL_PAGES } from "../misc/constants";
-import { generateOffsetUrl } from "../misc/helpers";
+import {
+  generateOffsetUrl,
+  getTotalPagesForPagination,
+  getPokemonIndexFromUrl,
+} from "../misc/helpers";
+import useSWR from "swr";
 
 export default function Home(props: any) {
-  const { pokemon } = props;
   const router = useRouter();
+  const [pokemon, setPokemon] = useState(props.pokemon.results);
   const [activePage, setPage] = useState(1);
 
   const handleClick = (pokemonName: string) => {
     router.push("./" + pokemonName);
   };
+
+  //@ts-ignore
+  const fetcher = (...args) => fetch(...args).then((r) => r.json());
+
+  const url = API_BASE_URL + generateOffsetUrl((activePage - 1) * 15);
+  const { data, error, isLoading } = useSWR(url, fetcher);
+
+  useEffect(() => {
+    if (data) {
+      setPokemon(data.results);
+    }
+  }, [data]);
+
+  if (error) return "An error has occurred.";
+  if (isLoading) return "Loading...";
 
   if (!pokemon)
     return (
@@ -37,12 +56,13 @@ export default function Home(props: any) {
       radius="md"
       className={"card"}
       onClick={() => handleClick(p.name)}
+      style={{ cursor: "pointer" }}
     >
       <AspectRatio ratio={1920 / 1080}>
         <Image
-          src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${
-            index + 1
-          }.png`}
+          src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${getPokemonIndexFromUrl(
+            p.url
+          )}.png`}
         />
       </AspectRatio>
 
@@ -60,7 +80,11 @@ export default function Home(props: any) {
       </Head>
       <main>
         <h1>Pokemon App</h1>
-        <Pagination value={activePage} onChange={setPage} total={TOTAL_PAGES} />
+        <Pagination
+          value={activePage}
+          onChange={setPage}
+          total={getTotalPagesForPagination(props.pokemon.count)}
+        />
 
         <div>
           <Container py="xl">
@@ -78,14 +102,14 @@ export default function Home(props: any) {
 
 export async function getStaticProps() {
   // Static Generation for only 15 items not good for SEO
-  const url = API_BASE_URL + generateOffsetUrl(15);
+  const url = API_BASE_URL + generateOffsetUrl(0);
 
   const res = await fetch(url);
   const data = await res.json();
 
   return {
     props: {
-      pokemon: data.results,
+      pokemon: data,
     },
 
     // TODO set revalidate
