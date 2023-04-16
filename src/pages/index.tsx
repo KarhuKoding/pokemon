5c623d13a9b4475fb0f8bdddce3d188ed1a9829e
JOPI-RTX3060\johan <johannes@walenta.dev>
2023-04-16 16:00:45 +0300

    basic get request

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,9 @@
 import Head from "next/head";
 
-export default function Home() {
+export default function Home(props: any) {
+  const { pokemon } = props;
+
+  console.log(pokemon);
   return (
     <>
       <Head>
@@ -12,4 +15,24 @@ export default function Home() {
       <main>Pokemon App</main>
     </>
   );
+}
+
+const BASE_URL = "https://pokeapi.co/api/v2/";
+const ALL_POKEMON = "pokemon?limit=100000&offset=0";
+const FIRST_15 = "pokemon?limit=15&offset=0";
+
+// TODO create helper function to generate URL 
+
+export async function getStaticProps() {
+  const url = BASE_URL + FIRST_15;
+
+  const res = await fetch(url);
+  const data = await res.json();
+
+  return {
+    props: {
+      pokemon: data.results,
+    },
+    // TODO set revalidate
+  };
 }
